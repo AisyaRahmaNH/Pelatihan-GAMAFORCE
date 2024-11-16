803f534ad78dc75fdb1f441d09baa72da5325523
JSometwo <mhafidzalfarisi543@gmail.com>
2024-11-17 05:40:10 +0700

    Finalisasi kode yang dipakai untuk tes terbang

--- ESP32IMURollPitch.ino
+++ ESP32IMURollPitch.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
 
-const int MPU = 0x68; // MPU6050 I2C address
+const int MPU = 0x68; //MPU6050 I2C address
 float AccX, AccY, AccZ, GyroX, GyroY, GyroZ;
 float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
 float roll, pitch, yaw;
@@ -9,14 +10,16 @@ float elapsedTime, currentTime, previousTime, logTime;
 float alertThreshold = 40;
 int c = 0;
 
+//Make SoftwareSerial object in pin GPIO17(TX) dan GPIO16 (RX)
+SoftwareSerial telemetrySerial(16, 17); // RX, TX
+
 void setup() {
-  Serial.begin(19200);
+  telemetrySerial.begin(57600);  
   Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);    
   Wire.write(0x00);    
   Wire.endTransmission(true);
-
   calculate_IMU_error();
   delay(20);
 }
@@ -28,11 +31,11 @@ void loop() {
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 6, true);
 
-  AccX = (int16_t)(Wire.read() << 8 | Wire.read()); // Read as int16_t then cast to float
+  AccX = (int16_t)(Wire.read() << 8 | Wire.read()); //Read as int16_t then cast to float
   AccY = (int16_t)(Wire.read() << 8 | Wire.read());
   AccZ = (int16_t)(Wire.read() << 8 | Wire.read());
 
-  // Convert to g-force (satuan gravitasi)
+  //Convert to g-force
   AccX = AccX / 16384.0;
   AccY = AccY / 16384.0;
   AccZ = AccZ / 16384.0;
@@ -59,26 +62,26 @@ void loop() {
   GyroY = (int16_t)(Wire.read() << 8 | Wire.read());
   GyroZ = (int16_t)(Wire.read() << 8 | Wire.read());
 
-  // Convert to deg/s
+  //Convert to deg/s
   GyroX = GyroX / 131.0;
   GyroY = GyroY / 131.0;
   GyroZ = GyroZ / 131.0;
 
-  // Correct the readings with error values
+  //Correct the readings with error values
   GyroX -= GyroErrorX;
   GyroY -= GyroErrorY;
   GyroZ -= GyroErrorZ;
 
-  // Calculate the gyro angles
+  //Calculate the gyro angles
   gyroAngleX += GyroX * elapsedTime;
   gyroAngleY += GyroY * elapsedTime;
   yaw += GyroZ * elapsedTime;
 
-  // Complementary filter
+  //Complementary filter
   roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
   pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
 
-  // Reset roll, pitch, and yaw if they exceed 180 degrees
+  //Reset roll, pitch, and yaw if they exceed 180 degrees
   if (roll > 180.0) {
     roll -= 360.0;
   } else if (roll < -180.0) {
@@ -97,32 +100,64 @@ void loop() {
     yaw += 360.0;
   }
 
-    // Mengirim alert jika roll melebihi threshold
+  //Showing logTime in second
+  logTime = currentTime/1000;
+
+  //Print values to Serial Monitor
+  telemetrySerial.print(logTime);
+  telemetrySerial.print("\tRoll: ");
+  telemetrySerial.print(roll);
+  telemetrySerial.print("\tPitch: ");
+  telemetrySerial.print(pitch);
+  telemetrySerial.print("\tYaw: ");
+  telemetrySerial.print(yaw);
+  telemetrySerial.print("\tGyroX: ");
+  telemetrySerial.print(GyroX);
+  telemetrySerial.print("\tGyroY: ");
+  telemetrySerial.print(GyroY);
+  telemetrySerial.print("\tGyroZ: ");
+
+  //Sending alert if roll exceeds threshold
   if (abs(roll) > alertThreshold) {
-    Serial.print(GyroZ);
-    Serial.println("\tALERT: Roll angle exceeds 40 degrees!");
+    telemetrySerial.print(GyroZ);
+    telemetrySerial.print("\tALERT: Roll angle exceeds ");
+    telemetrySerial.print(alertThreshold);
+    telemetrySerial.println(" degrees!");
   }
 
   else{
-    Serial.println(GyroZ);
+    telemetrySerial.println(GyroZ);
   }
 
-  //Definisi logtime dalam second untuk ditampilkan
-  logTime = currentTime/1000;
+  //Checking if there are input to change threshold
+  if (telemetrySerial.available()) {
+    char input = telemetrySerial.read();
+    if (input == 'w') {
+      alertThreshold += 5;
+      if (alertThreshold > 90){ 
+      alertThreshold = 90; //Maximal
+      telemetrySerial.print("Threshold is at maximum: ");
+      telemetrySerial.println(alertThreshold);
+      }
+      else{
+      telemetrySerial.print("Threshold increased to: ");
+      telemetrySerial.println(alertThreshold);
+      }
+    } 
+    else if (input == 's') {
+      alertThreshold -= 5;
+      if (alertThreshold < 5){ 
+      alertThreshold = 5; //Minimal
+      telemetrySerial.print("Threshold is at minimum: ");
+      telemetrySerial.println(alertThreshold);
+      }
+      else{
+      telemetrySerial.print("Threshold decreased to: ");
+      telemetrySerial.println(alertThreshold);
+      }
+    }
+  }
 
-  // Print values to Serial Monitor
-  Serial.print(logTime);
-  Serial.print("\tRoll: ");
-  Serial.print(roll);
-  Serial.print("\tPitch: ");
-  Serial.print(pitch);
-  Serial.print("\tYaw: ");
-  Serial.print(yaw);
-  Serial.print("\tGyroX: ");
-  Serial.print(GyroX);
-  Serial.print("\tGyroY: ");
-  Serial.print(GyroY);
-  Serial.print("\tGyroZ: ");
 }
 
 void calculate_IMU_error() {
